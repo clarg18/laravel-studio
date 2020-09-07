1d8657209f5e91e2dec7af02405144d75f95d617
Glen Clark <glenuk@gmail.com>
2020-09-07 09:03:37 +0100

    No longer creates composer dir by default

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp example.env .env
 mkdir ./src
-mkdir $(grep COMPOSER_HOME= .env | cut -d= -f2)
+#mkdir $(grep COMPOSER_HOME= .env | cut -d= -f2)
 sed -i "/HOST_UID=/c\HOST_UID=$(id -u)" .env
 sed -i "/HOST_GID=/c\HOST_GID=$(id -g)" .env

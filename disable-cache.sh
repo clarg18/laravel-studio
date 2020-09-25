24500f80cdfdac4af2b5603ab0135647def6706a
Glen Clark <glenuk@gmail.com>
2020-09-25 10:59:31 +0100

    script does not replace whole line now

--- disable-cache.sh
+++ disable-cache.sh
@@ -3,5 +3,5 @@
 sed -i "/COMPOSER_CONFIG/c\# COMPOSER_CONFIG" .env
 sed -i "/COMPOSER_CACHE_DIR/c\# COMPOSER_CACHE_DIR" .env
 
-sed -i '/- "${COMPOSER_CACHE_DIR}/c\#- "${COMPOSER_CACHE_DIR}' docker-conpose.yml
-sed -i '/- "${COMPOSER_CONFIG}/c\#- "${COMPOSER_CONFIG}' docker-conpose.yml
+sed -i '/- "${COMPOSER_CACHE_DIR}/#- "${COMPOSER_CACHE_DIR}' docker-compose.yml
+sed -i '/- "${COMPOSER_CONFIG}/#- "${COMPOSER_CONFIG}' docker-compose.yml

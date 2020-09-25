5c0b66d3e686e5d16f12138e706bb5ae1c2485e8
Glen Clark <glenuk@gmail.com>
2020-09-25 11:29:15 +0100

    fixed sctipts

--- disable-cache.sh
+++ disable-cache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "/COMPOSER_CONFIG/c\# COMPOSER_CONFIG" .env
-sed -i "/COMPOSER_CACHE_DIR/c\# COMPOSER_CACHE_DIR" .env
+sed -i "s/COMPOSER_CONFIG/# COMPOSER_CONFIG/" .env
+sed -i "s/COMPOSER_CACHE_DIR/# COMPOSER_CACHE_DIR/" .env
 
-sed -i '/- "${COMPOSER_CACHE_DIR}/#- "${COMPOSER_CACHE_DIR}' docker-compose.yml
-sed -i '/- "${COMPOSER_CONFIG}/#- "${COMPOSER_CONFIG}' docker-compose.yml
+sed -i 's/- "${COMPOSER_CACHE_DIR}/#- "${COMPOSER_CACHE_DIR}/' docker-compose.yml
+sed -i 's/- "${COMPOSER_CONFIG}/#- "${COMPOSER_CONFIG}/' docker-compose.yml
--- enable-cache.sh
+++ enable-cache.sh
@@ -3,5 +3,5 @@
 sed -i "/# COMPOSER_CONFIG/c\COMPOSER_CONFIG" .env
 sed -i "/# COMPOSER_CACHE_DIR/c\COMPOSER_CACHE_DIR" .env
 
-sed -i '/#- "${COMPOSER_CACHE_DIR}/- "${COMPOSER_CACHE_DIR}' docker-compose.yml
-sed -i '/#- "${COMPOSER_CONFIG}/- "${COMPOSER_CONFIG}' docker-compose.yml
+sed -i 's/#- "${COMPOSER_CACHE_DIR}/- "${COMPOSER_CACHE_DIR}/g' docker-compose.yml
+sed -i 's/#- "${COMPOSER_CONFIG}/- "${COMPOSER_CONFIG}/g' docker-compose.yml

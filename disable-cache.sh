d10b98aa73a8f2c75307a6febeb67b126f2ea2d8
clarg18 <glenuk@gmail.com>
2022-01-29 21:18:38 +0000

    disbale-composer-cache no longer adds additional #s

--- disable-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i "s/COMPOSER_CONFIG/# COMPOSER_CONFIG/" .env
-sed -i "s/COMPOSER_CACHE_DIR/# COMPOSER_CACHE_DIR/" .env
-
-sed -i 's/- "${COMPOSER_CACHE_DIR}/#- "${COMPOSER_CACHE_DIR}/' docker-compose.yml
-sed -i 's/- "${COMPOSER_CONFIG}/#- "${COMPOSER_CONFIG}/' docker-compose.yml
